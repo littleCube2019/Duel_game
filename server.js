cd451334bc0894a524d9450e4f57e090150afa09
littleCube2019 <willyfang09@gamil.com>
2021-02-05 14:55:12 +0800

    conflict remove

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
@@ -23,57 +22,30 @@ var player2 = {"hp":10, "atk":1, "def":1, "crit_rate":0.5, "action":"none",
 var ready1 = false, ready2 = false;
 var act_ready1=false, act_ready2=false;
 var item_ready1 = false, item_ready2 = false;
-=======
-'use strict';
 
-const express = require('express');
-const socketIO = require('socket.io');
-
-const PORT = process.env.PORT || 3000;
-const INDEX = '/index.html';
-
-const server = express()
-  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
-
-const io = socketIO(server);
-
-var player = {"hp":10, "atk":1, "def":1, "crit_rate":0.5};
-var ready1 = false, ready2 = false;
->>>>>>> 8926358bb1aace458357db70fb3b7bf67feca250
 
 io.on('connection', (socket) => {
   socket.emit("welcome");
   console.log('Client connected');
   socket.on('disconnect', () => console.log('Client disconnected'));
-<<<<<<< HEAD
+
 
   //choose player
   socket.on("choose_character", (pl)=>{
-=======
-  socket.on("update", ()=>{
-    console.log("update" + player.atk);
-    socket.emit("update", player.hp, player.atk, player.def, player.crit_rate);
-  })
-  socket.on("choose", (pl)=>{
->>>>>>> 8926358bb1aace458357db70fb3b7bf67feca250
+
     if(pl==1){
       console.log("pl1 has been choosed");
       ready1 = true;
       socket.emit("id_pl1");
       io.emit("pl1_choosed");
-<<<<<<< HEAD
-=======
-      if(ready2){
-        io.emit("start_game");
-      }
->>>>>>> 8926358bb1aace458357db70fb3b7bf67feca250
+
+
     }else if(pl==2){
       console.log("pl2 has been choosed");
       ready2 = true;
       socket.emit("id_pl2");
       io.emit("pl2_choosed");
-<<<<<<< HEAD
+
     }
     if(ready1 && ready2){
       io.emit("start_game", 10, 1, 1, 0.5);
@@ -167,14 +139,7 @@ io.on('connection', (socket) => {
         player2.item.value = 0;
         socket.emit("delete_item");
       }
-=======
-      if(ready1){
-        io.emit("start_game");
-      }
-    }
-    if(ready1 && ready2){
-      console.log("start_game");
->>>>>>> 8926358bb1aace458357db70fb3b7bf67feca250
+
     }
   })
 });
