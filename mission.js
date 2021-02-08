6aba8904fb292d81f072ee1d83c222abb77b1a89
littleCube2019 <willyfang09@gamil.com>
2021-02-08 14:55:23 +0800

    temp

--- mission.js
+++ mission.js
@@ -1,7 +1,10 @@
-/* mission foretermine player can accept this mission
-let checker = (arr, target) => target.every(v => arr.includes(v));
+
+function canAcceptMission(player,mission){
+// input :player object , mission object
+// output: boolean , determine player can accept this mission or not
+	let checker = (arr, target) => target.every(v => arr.includes(v));
 // check all target ele in arr ? 
-return checker(player.doneMission,preneed_id);
+	return checker(player.doneMission,mission.preneed_id);
 
 }
 
@@ -20,7 +23,7 @@ var mission = [
 			mission_success(player,enemy);
 		}
 		else{
-			if(player.action==1){ //action == defend
+			if(player.action=="def"){ //action == defend
 				player.remaining-=1;
 			}
 		}
@@ -42,13 +45,12 @@ var mission = [
 		player.remaining = 3;
 	},
 	mission_check:(player,enemy)=>{
-		if(player.remaining==0){
+		if(player.remaining<0){
 			mission_success(player,enemy);
 		}
 		else{
-			if(player.action==1){ //action == defend
-				player.remaining-=1;
-			}
+			player.remaining-=player.damageNoDef["spike"] ;
+			//造成三點荊棘害傷
 		}
 		
 	},
@@ -61,5 +63,69 @@ var mission = [
 		player.mission=-1; //mission remove 
 	},
 },
-
+{
+	id:2,
+	preneed_id:[],
+	mission_start:(player,enemy)=>{
+		player.remaining = 3;
+	},
+	mission_check:(player,enemy)=>{
+		if(player.remaining==0){
+			mission_success(player,enemy);
+		}
+		else{
+			if(player.action=="def"){ //action == defend
+				player.remaining-=1;
+			}
+		}
+		
+	},
+	mission_fail:(player,enemy)=>{ // when violate the rule, or *discard* 
+		player.remaining=0; 
+		player.mission=-1; //mission remove
+	},
+	mission_success:(player,enemy)=>{
+		player.def+=1;
+		player.mission=-1; //mission remove 
+	},
+},
+{
+	id:3,
+	preneed_id:[2],
+	mission_start:(player,enemy)=>{
+		player.remaining = 3;
+	},
+	mission_check:(player,enemy)=>{
+		if(player.remaining==0){
+			mission_success(player,enemy);
+		}
+		else{
+			if(remaining==3){
+				if(player.action.basic=="def" ){ //action == defend
+					player.remaining-=1;
+				}
+				else{
+					mission_fail(player,enemy);
+				}
+			}
+			else{
+				if(player.action.basic=="def" ){ //action == defend
+					player.remaining-=1;
+				}
+				else{
+					mission_fail(player,enemy);
+				}
+			}
+		}
+		
+	},
+	mission_fail:(player,enemy)=>{ // when violate the rule, or *discard* 
+		player.remaining=0; 
+		player.mission=-1; //mission remove
+	},
+	mission_success:(player,enemy)=>{
+		player.def+=1;
+		player.mission=-1; //mission remove 
+	},
+},
 ]
