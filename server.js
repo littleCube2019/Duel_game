eedf9784ef4329a2e9afcf8a8ac5be571fc06d88
brad1024 <harry1024harry1024@gmail.com>
2021-02-20 15:59:33 +0800

    20210220

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(express.static('public'));
 
 var missiontype = 5;
 var itemNum = 5;
-var missionIdToIndex={
+var missionIdToIndex={ //共23張 (0~22)
   10000:0,
   10001:1,
   10010:2,
@@ -38,39 +38,99 @@ var missionIdToIndex={
   10040:9,
   10041:10,
   10022:11,
+  10050:12,
+  10060:13,
+  10070:14,
+  10080:15,
+  10090:16,
+  10100:17,
+  10110:18,
+  10120:19,
+  10130:20,
+  10140:21,
+  10150:22,
 }
+const numOfSerialMission =  16;  //看上面object最大值的 千百十 +1
+const numOfItem =  6;  //道具總數
+var initailNotAvaiableMission =[11,12,13]; 
+//用在如  神與天罰系列在一開始不應被選到，解完基礎任務後會依次打開，但又不是系列關係
+//的任務奇怪關係上
 
 class player {
 	constructor(playerId){
 			// 以一個基本初始玩家為預設值
   this.id = playerId;  //區分玩家
-  this.hp = 10; //血量 
+  this.hp = 4; //血量 
   this.maxHp = 10; // 最大血量
   this.atk = 1; //攻擊力
   this.def = 1; //防禦力
   this.crit_rate = 0.5 //爆擊率
 
   this.item = -1;  // 應該會放item卡的id
-  this.sprite = -1; //同上 精靈卡的id
+  this.item2 = -1;  //第二個道具欄
+  this.itemRecord=[]; //紀錄道具使用次數
+  for(var temp=0; temp<numOfItem; temp++){
+      this.itemRecord.push(0); //所有道具一開始使用次數皆為0
+  }  
+
+
+
+  this.sprite = -1;   //精靈id
+  this.spriteHp=0;
+
   this.mission = -1; //同上
-  this.nextMissionAvailable = [0, 0, 0, 0]; //新增
+  
+
+  this.prayHealth=0; //祈禱回血量
+  this.prayRemaining=0; //祈禱效果影響時長
+
+  this.nextMissionAvailable =  [];  //系列任務
+  for(var temp=0; temp<numOfSerialMission; temp++){
+    if(initailNotAvaiableMission.includes(temp)){
+        this.nextMissionAvailable.push(-1); //如果在不應一開始被抽到的任務，設為-1
+    }
+    else{
+      this.nextMissionAvailable.push(0);  //其他就設為0，可以被選到起始任務
+    }
+  }  
+
+
   this.action = {"basic":"none", "item":"none", "card":"none"}; //使用者該回合採取的行動 (可能可以分為 0: 攻擊, 1:防守, 2:祈禱 ....)
   this.prevAction= "none"; //上一回合的行動
-  this.damageDef={"normal": 0, "sprite":0 } //會計算對方防禦的傷害
-  this.damageNoDef={"spike":0 , "item":0} // 不會計算對方防禦的傷害
-  this.takenDamage={"normal": 0, "sprite":0 , "spike":0 , "item":0} // 承受傷害
-  this.remaining=0;
+  this.damageDef={"normal": 0, "sprite":0 , "item":0 } //會計算對方防禦的傷害
+  this.damageNoDef={"spike":0} // 不會計算對方防禦的傷害
+  this.takenDamage={"normal": 0 , "spike":0, "sprite":0 , "item":0} // 承受傷害
+  
+  this.remaining=0; //任務用暫存計數變數
+  this.tempObj={}; //任務用暫存物件
+
+  
   this.isCritical = false; // 是否爆擊
   this.actionReady = {"basic":false, "mission":false}; //是否完成一回合的行動
-  this.state = {"stun":false};
+  this.state ={
+                "stun":false,
+                "rage":false,
+                "undeath":false,
+                "suckBlood":false,
+                "sprite_sacrifice":false,
+                "sprite_snail":false, 
+                "canPray":false ,
+                "canRedBless":false,
+                "canBlueBless":false,
+                "secondItem":false,
+                "thief":false, }; //玩家狀態
+  this.equip = false; //道具的裝備卡使用
   }
+
+
+
   getAction(action, item, card){
     this.prevAction = this.action.basic;
     this.action.basic = action;
     this.action.item = item;
     this.action.card = card;
   }
-
+  
   // 造成傷害(未計算防禦)
   totalDamage(){
     if(this.action.basic=="atk"){
@@ -83,6 +143,13 @@ class player {
     }else{
       this.damageDef.normal = 0;
     }
+    /*
+    if(this.action.item=="use"){
+      this.damageDef.item = this.item; //待補
+    }else{
+      this.damageDef.item = 0;
+    }
+    */
   }
 
   // 實際總傷害:
@@ -111,7 +178,7 @@ class player {
     }else{
       enemy.takenDamage.spike = this.damageNoDef.spike;
     }
-    console.log(this.id + " 荊棘:" + this.damageNoDef.spike + " 傷害:" + sumNoDef + " 敵人行動:" + enemy.action.basic);
+    //console.log(this.id + " 荊棘:" + this.damageNoDef.spike + " 傷害:" + sumNoDef + " 敵人行動:" + enemy.action.basic);
     return sumDef + sumNoDef;
   }
 
@@ -183,6 +250,7 @@ function missionAction(action, me, enemy)
       mission[me.mission].mission_start(me, enemy);
       io.emit("mission_state", me, missionCard[me.mission], "start");
       console.log(me.id + " " + cardId);
+      me.actionReady.mission = true;
     }else if(cardId>=20000){
       me.item = cardId-20000;
       io.emit("item_state", me, itemCard[me.item], "get");
@@ -231,6 +299,7 @@ function playerStun(me, enemy){
       console.log("玩家" + enemy.id + "沒有被擊暈");
     }
 }
+
 
 function isGameOver(player1, player2){
   if(player1.hp<=0 || player2.hp<=0){
@@ -285,41 +354,47 @@ io.on('connection', (socket) => {
       player2.actionReady.basic = true;
       console.log("player2 done");
     }
-    if(player1.actionReady.basic && player2.actionReady.basic){
+
+    //==================================================回合結算======================================================================
+    if((player1.actionReady.basic || player1.state.stun) && (player2.actionReady.basic || player2.state.stun)){
       var p1top2, p2top1;
+      //======================================================道具結算=================================================================
       itemAction(player1.action.item, player1, player2);
       itemAction(player2.action.item, player2, player1);
+      //=======================================================行動結算 && 精靈結算====================================================
       if(!player1.state.stun){
         player1.totalDamage();
         p1top2 = player1.realDamage(player2);
       }
-      player1.totalDamage();
-      p1top2 = player1.realDamage(player2);
-      player2.totalDamage();
-      p2top1 = player2.realDamage(player1);
+      if(!player2.sprite.stun){
+        player2.totalDamage();
+        p2top1 = player2.realDamage(player1);
+      }
+      //========================================================傷害結算===============================================================
       player1.takeDamage(p2top1);
       player2.takeDamage(p1top2);
       //血量判定
       io.emit("dmg", p1top2, p2top1, player1.action.basic, player2.action.basic);
       io.emit("next_round", player1, player2);
 
+      //=======================================================判斷輸贏================================================================
       if(!isGameOver(player1, player2)){
+        //=====================================================任務檢查====================================================================
         missionAction("check", player1, player2);
         missionAction("check", player2, player1);
         console.log("p1 mission remain: " + player1.remaining + " p2 mission remain: " + player2.remaining);
 
+        //=====================================================狀態還原====================================================
         player1.actionReady.basic = player1.actionReady.mission = player1.state.stun = false;
         player2.actionReady.basic = player2.actionReady.mission = player2.state.stun = false;
 
-        player1.state.stun = player2.state.stun = false;
+        //====================================================狀態======================================================================
         playerStun(player1, player2);
         playerStun(player2, player1);
         io.emit("get_stuned", player1.state.stun, player2.state.stun);
       }
-      
+
     }
   })
-});
-
-
+})
 
